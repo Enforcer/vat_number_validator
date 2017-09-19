fe9b0ccba059539bc24c14e232b61cedcbc8fc0b
Sebastian Buczyński <nnplaya@gmail.com>
2017-09-19 21:35:48 +0200

    Added another test to be more certain everything works

--- tests.py
+++ tests.py
@@ -11,6 +11,14 @@ class SeparatingCountryPrefixTest(unittest.TestCase):
         self.assertEqual(country_code, 'CZ')
         self.assertEqual(number, '28987373')
 
+    def test_should_separate_more_complicated_country_code_and_number(self):
+        vat_number = 'NL802465602B01'
+
+        country_code, number = get_country_code_and_number_from_vat_number(vat_number)
+
+        self.assertEqual(country_code, 'NL')
+        self.assertEqual(number, '802465602B01')
+
 
 if __name__ == '__main__':
     unittest.main()
